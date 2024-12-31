3ffade63a8a64c327eaad4de3219a41171f1ab7d
CelularBat <105959781+CelularBat@users.noreply.github.com>
2024-12-31 10:03:17 +0100

    upd

--- dev_scripts/userScriptConfig.js
+++ dev_scripts/userScriptConfig.js
@@ -2,8 +2,9 @@
 // @name         WebPainter
 // @namespace    http://tampermonkey.net/
 // @version      2024-12-25
-// @description  try to take over the world!
+// @description  Paint on any webpage !
 // @author       CelularBat
+// @homepage     https://github.com/CelularBat/WebPainter_userscript
 // @match        *://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
